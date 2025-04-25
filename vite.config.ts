62ffd6582fc930d1f7c1e132864ec4364bf01110
maximvarentsov <maksim.v@ohm.agency>
2025-04-25 18:00:44 +0200

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [react()]
-})
+  root: '.',                     // указываем корень
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  },
+  publicDir: 'public'
+});
