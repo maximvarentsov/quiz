0bcc04b30e18101dd2012a557318858d56423a7e
maximvarentsov <maksim.v@ohm.agency>
2025-04-25 17:54:49 +0200

    Make project for vercel.com

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import FinancialProductQuiz from './financial-quiz';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <FinancialProductQuiz />
+  </React.StrictMode>
+);
